701184d376df366e95160324ec2303f22c5e6789
Burcu Kacak <burcu.kacakk@gmail.com>
2022-12-17 15:27:21 +0300

    1.3

---
+++ src/main/java/day12forloopwhileloop/ForLoop05.java
@@ -0,0 +1,39 @@
+package day12forloopwhileloop;
+
+public class ForLoop05 {
+    public static void main(String[] args) {
+
+        //Hic sayi kullanmadan 1'den 100'e kadar olan sayilari konsola yazdiriniz
+
+        for (int i = 'd'/'d'; i <='d'; i++) {
+            System.out.print(i +" - ");
+
+        }
+    //Note 1; Bazi loop'lar hic calismayabilir
+
+        for (int i =1 ; i <0 ; i++) {
+            System.out.println("Hi");// Zero execution
+
+    //Note 2;Bazi loop'lar sonsuz defa calisabilir
+    //       Bu tarz loop'lara "Infinite(sonsuz) Loop denir.//
+    //      "Infinite (Sonsuz) Loop"genellikle "incremet/decrement"kisminda yapilan hatadan kaynaklanir
+            //for (int k = 0; k <10 ; k--) {
+            //System.out.println("Java is money");
+
+
+            //Note 3: Baska bir "Infinite( Sonsuz) Loop"
+            //Loop olusturdugunuzda "ikinci kismi " yazmazsaniz Infinite(Sonsuz) Loop olur.
+            //for (int l = 1;  ; ) {
+              //  System.out.println("Hi!");}
+
+
+            }
+
+        }
+
+
+
+
+
+
+}
---
+++ src/main/java/day12forloopwhileloop/WhileLoop01.java
@@ -0,0 +1,54 @@
+package day12forloopwhileloop;
+
+public class WhileLoop01 {
+
+    public static void main(String[] args) {
+
+        //Example1: 3'ten 10'a kadar tamsayilari konsola yazdiriniz
+
+        for (int i = 3; i <11 ; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        //2.Yol While Loop
+
+        int i = 3;
+        while (i<11){
+
+            System.out.print(i + " ");
+
+            i++;
+        }
+        System.out.println();
+        //Example1: 23'ten 12'ye kadar cift tamsayilari konsola yazdiriniz
+
+        int k = 23;
+
+        while (k>11){
+            if (k %2 == 0){
+                System.out.print(k + " ");
+            }
+            k--;
+
+        }
+        System.out.println();
+        //Example 3: Verilen bir tam sayinin rakamlari toplamini konsola yazdiran kodu yaziniz
+        int num = -578;
+
+        num = Math.abs(num);
+        int sum = 0;
+
+        while(num>0){
+            sum = sum + num%10;
+            num/=10;
+        }
+        System.out.println(sum);
+
+
+
+
+
+    }
+
+
+}
