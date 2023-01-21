d91f2d8b713b73780ed59f31ba35dfd7d11fe242
Burcu Kacak <burcu.kacakk@gmail.com>
2023-01-21 17:59:27 +0300

    version2.3

--- src/main/java/day27encapsulationabstraction/CoursesRunner.java
+++ src/main/java/day27encapsulationabstraction/CoursesRunner.java
@@ -5,9 +5,10 @@ public class CoursesRunner {
 
 
         //"abstract class"larin icinde "abstract method"lar vardir.
+        // Class abstract olduktan sonra abstract method koymak zorunda degiliz
         // "abstract method"larin body'si olmadigindan bu method"lar yarim method sayilir.
-        // Dolayisiya; abstract class'lar eksigi olan kaliplar gibi dusunulebilir.
-        //Java eksik kaliplarin object uretmesine musaade etmez.
+        // Dolayisiyla; abstract class'lar eksigi olan kaliplar gibi dusunulebilir.
+        // Java eksik kaliplarin object uretmesine musaade etmez.
         //Bu yuzden, "abstract class"lardan object uretilemez.
 
         //"abstract class"larin constructor 'i vardr ama bu constructor object uretemez haldedir.
--- src/main/java/day27encapsulationabstraction/StudentRunner.java
+++ src/main/java/day27encapsulationabstraction/StudentRunner.java
@@ -1,6 +1,7 @@
 package day27encapsulationabstraction;
 
 public class StudentRunner {
+
     public static void main(String[] args) {
 
         Student myStd = new Student();
@@ -11,21 +12,12 @@ public class StudentRunner {
         System.out.println(myStd.isPoor());
 
         //Student 2
-        myStd.setStdId("TH123");
-        myStd.setGpa(4.00);
-        myStd.setPoor(false);
-
-
-        String stdId = myStd.getStdId();
-        myStd.getStdId();// Bu bize Variable degerini verecek.
-
-        System.out.println(stdId);//AC123
+        myStd.setStdId("TH123");// Bu bize Variable degerini verecek.
         myStd.setGpa(4.0);//encapsule yapilmis variable'i set methodu ile degistirdik
-        //set method kalibi degistirmez objedeki degeri degistirir.
-        //Encapsulation faydalarindan biri de tek bir obje olusturup set methodu ile istedigimiz objeye cevirebiliriz
-
-        System.out.println(myStd.getGpa());
-
-
+        myStd.setPoor(false);
     }
 }
+
+//set method kalibi degistirmez objedeki degeri degistirir.
+//Encapsulation faydalarindan biri de tek bir obje olusturup set methodu ile istedigimiz objeye cevirebiliriz
+
